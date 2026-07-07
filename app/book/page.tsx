2a17f7c1dd8e4d04a10513fc0d89cbad364fbe3f
Raechel Odom <raechelodom@Raechels-MacBook-Pro.local>
2026-07-06 21:52:13 -0600

    adds favicon, title

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -20,3 +20,22 @@ const BookPage = () => {
 };
 
 export default BookPage;
+
+// NEXT STEPS:
+// set up database with Neon
+// allow user to create + save polls
+// display poll results & allow users to vote
+// allow user to vote on next book to read
+// display currently selected book on page
+// change format of the book card
+// tweak filters again (seem to be excluding too much)
+// create individual game pages
+// allow book clubs to be created/hosted*
+// create calendar page with book club events
+// add book club host to mark book as done*
+// allow book club host to create events
+// allow users to add events to their google calendar*
+// show completed books on the past reads page
+// allow users to view polls/book bingos for their past reads with a 'view challenges' button/page
+
+// * lower importance
